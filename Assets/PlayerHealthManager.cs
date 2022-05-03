13d80107a3710466373a0f24fed0be66847a3e93
Tyler Denman <tylerdenman200@gmail.com>
2022-05-03 16:17:41 +0100

    Add Unity Files

---
+++ Assets/PlayerHealthManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    public float health;
+    [SerializeField] private Image HealthBarImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+        health = 1f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthBarImage.fillAmount = health;
+
+        if (HealthBarImage.fillAmount <= 1f)
+        {
+            HealthBarImage.color = Color.green;
+        }
+        if (HealthBarImage.fillAmount <= 0.7f)
+        {
+            HealthBarImage.color = Color.yellow;
+        }
+        if (HealthBarImage.fillAmount <= 0.3f)
+        {
+            HealthBarImage.color = Color.red;
+        }
+    }
+}
---
+++ Assets/menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class menu : MonoBehaviour
+{
+    public Button StartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
